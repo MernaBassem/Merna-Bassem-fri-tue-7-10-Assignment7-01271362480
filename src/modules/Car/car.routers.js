1ee8e8bd3c4fbc5292a0eab612b12a595067047b
Merna Bassem <mernabassem21@gmail.com>
2024-06-19 21:34:46 +0300

    Get Available Cars of a Specific Models

--- src/modules/Car/car.routers.js
+++ src/modules/Car/car.routers.js
@@ -7,5 +7,9 @@ router.get("/specificCarById/:id", CarController.specificCarById);
 router.get("/getAllCar", CarController.getAllCar);
 router.put("/updateCar/:id", CarController.updateCar);
 router.delete("/deleteCar/:id", CarController.deleteCar);
+router.get(
+  "/availableCarSpecificModel",
+  CarController.availableCarSpecificModel
+);
 
 export default router;
