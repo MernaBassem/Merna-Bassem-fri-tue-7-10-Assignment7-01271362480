472e049452cad799e4838702947aef874ffcdfa5
Merna Bassem <mernabassem21@gmail.com>
2024-06-19 15:56:28 +0300

    get specific car by id

--- src/modules/Car/car.controller.js
+++ src/modules/Car/car.controller.js
@@ -1,6 +1,7 @@
 import { Car } from "../../../DB/connection.js";
+import { ObjectId } from "mongodb";
 
-// add Car
+// 1- add Car
 export const addCar = async (req, res, next) => {
   const { name, model, rental_status } = req.body;
   const existingCar = await Car.findOne({ name, model });
@@ -17,6 +18,22 @@ export const addCar = async (req, res, next) => {
       rental_status,
     });
     return res.status(201).json({ message: "Successfully ADD Car ", car });
+  } catch (error) {
+    return res.status(400).json({ message: error.message });
+  }
+};
+
+//----------------------------------------------------------------
+//2- Get a specific car.
+
+export const specificCarById = async (req, res, next) => {
+  const { id } = req.params;
+  try {
+    const car = await Car.findOne({ _id: new ObjectId(id) });
+    if (!car) {
+      return res.status(404).json({ message: "Car not found" });
+    }
+    return res.status(200).json({ car });
   } catch (error) {
     return res.status(400).json({ message: error.message });
   }
--- src/modules/Car/car.routers.js
+++ src/modules/Car/car.routers.js
@@ -3,5 +3,6 @@ import * as CarController from "./car.controller.js";
 const router = Router();
 
 router.post("/addCar",CarController.addCar);
+router.get("/specificCarById/:id", CarController.specificCarById);
 
 export default router;
