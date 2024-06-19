ee08e9e9477fae94a8599fd15db61441911e7fc4
Merna Bassem <mernabassem21@gmail.com>
2024-06-19 20:06:55 +0300

    update rental

--- src/modules/Rental/rental.controller.js
+++ src/modules/Rental/rental.controller.js
@@ -11,7 +11,7 @@ export const addRental = async (req, res, next) => {
       _id: new ObjectId(UserId),
     });
     console.log("User Data", checkUserID);
-    if (checkUserID==null) {
+    if (checkUserID == null) {
       return res.status(401).json({ message: "Car Or User Not Found" });
     }
     // check car found
@@ -19,7 +19,7 @@ export const addRental = async (req, res, next) => {
       _id: new ObjectId(carId),
     });
     console.log("Car Data", checkCarID);
-    if (checkCarID==null) {
+    if (checkCarID == null) {
       return res.status(401).json({ message: "Car Or User Not Found" });
     }
     //check car available
@@ -44,6 +44,81 @@ export const addRental = async (req, res, next) => {
     return res
       .status(201)
       .json({ message: "Successfully ADD Rental ", addRental });
+  } catch (error) {
+    return res.status(400).json({ message: error.message });
+  }
+};
+//----------------------------------------------------
+//update rental
+// 1- update rental
+
+export const UpdateRental = async (req, res, next) => {
+  const { carId, UserId, rental_date, return_date } = req.body;
+  const { id } = req.params;
+
+  try {
+    const rentalData = await Rental.findOne({
+      _id: new ObjectId(id),
+    });
+    console.log("rental Dataaaaaaaaa",rentalData)
+    if (rentalData==null) {
+      return res.status(404).json({ message: "rental not found." });
+    }
+
+    const updateFields = {};
+
+    if (rental_date) updateFields.rental_date = rental_date;
+    if (return_date) updateFields.return_date = return_date;
+    // check user found
+    if (UserId) {
+      updateFields.UserId = UserId;
+      const checkUserID = await User.findOne({
+        _id: new ObjectId(UserId),
+      });
+      console.log("User Data", checkUserID);
+      if (checkUserID == null) {
+        return res.status(401).json({ message: "Car Or User Not Found" });
+      }
+    }
+
+    // check car found
+    if (carId) {
+      updateFields.carId = carId;
+      const checkCarID = await Car.findOne({
+        _id: new ObjectId(carId),
+      });
+      console.log("Car Data", checkCarID);
+      if (checkCarID == null) {
+        return res.status(401).json({ message: "Car Or User Not Found" });
+      }
+      //check car available
+      if (checkCarID.rental_status === "rented") {
+        return res
+          .status(401)
+          .json({ message: "This car is not available for rent" });
+      }
+
+      const updateCar = await Car.updateOne(
+        {
+          _id: new ObjectId(rentalData.carId),
+        },
+        { $set: { rental_status: "available" } }
+      );
+    }
+
+    const updateRental = await Rental.updateOne(
+      { _id: new ObjectId(id) },
+      { $set: updateFields }
+    );
+    const updateCar = await Car.updateOne(
+      {
+        _id: new ObjectId(carId),
+      },
+      { $set: { rental_status: "rented" } }
+    );
+    return res
+      .status(201)
+      .json({ message: "Successfully Update Rental ", UpdateRental });
   } catch (error) {
     return res.status(400).json({ message: error.message });
   }
--- src/modules/Rental/rental.routers.js
+++ src/modules/Rental/rental.routers.js
@@ -5,7 +5,7 @@ const router = Router();
 router.post("/addRental", RentalController.addRental);
 // router.get("/specificCarById/:id", CarController.specificCarById);
 // router.get("/getAllCar", CarController.getAllCar);
-// router.put("/updateCar/:id", CarController.updateCar);
+router.put("/UpdateRental/:id", RentalController.UpdateRental);
 // router.delete("/deleteCar/:id", CarController.deleteCar);
 
 export default router;
